b5323beec183cc15c1d08824cefdb8011efdf2ce
Priyanka <user@RCR-R90GPGYG>
2019-11-18 17:14:01 -0500

    Added Customers table

---
+++ sql_workbooks/Customers.sql
@@ -0,0 +1,28 @@
+create table Customers(
+CustomerId NUMBER,
+FirstName VARCHAR(20),
+LastName VARCHAR(20),
+EmailId VARCHAR(20),
+State VARCHAR(20),
+Password VARCHAR(20),
+ReatailorId NUMBER,
+Amount NUMBER
+);
+
+SELECT * FROM Customers;
+
+INSERT INTO CUSTOMERS VALUES(1, 'john', 'cena', 'johncena@mail.com', 'Virginia', 'jona', 100, 1000);
+
+INSERT INTO CUSTOMERS VALUES(2, 'dan', 'smith', 'dansmith@mail.com', 'Milford', 'dath', 101, 1500);
+INSERT INTO CUSTOMERS VALUES(3, 'peter', 'pan', 'peterpan@mail.com', 'Delaware', 'pean', 100, 1100);
+INSERT INTO CUSTOMERS VALUES(4, 'sandra', 'anders', 'sandranders@mail.com', 'Florida', 'sars', 101, 2000);
+INSERT INTO CUSTOMERS VALUES(5, 'marian', 'gold', 'mariangold@mail.com', 'California', 'mald', 102, 2500);
+INSERT INTO CUSTOMERS VALUES(6, 'kim', 'wild', 'kimwild@mail.com', 'Kentucy', 'kild', 102, 2005);
+
+select * from Customers;
+
+
+
+
+
+
