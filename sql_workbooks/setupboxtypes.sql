fb4edfc4833244eba594352699e9fad5cfe5a995
Ridhima Joshi <ridhima1005@users.noreply.github.com>
2019-11-19 14:52:01 -0500

    Modified STBType Table defination

--- sql_workbooks/setupboxtypes.sql
+++ sql_workbooks/setupboxtypes.sql
@@ -1,22 +1,17 @@
 CREATE table SetUpBoxTypesBox (
-	ID number(10,0) PRIMARY KEY,
-	Dimensions nvarchar2(255),
-	Type number(3,0),
-	Features nvarchar2(255),
-	Price number(10,2),
-	UpgrationCharges number(10,2),
-	Discount number(5,2),
-	BillingType nvarchar2(255),
-	RefundableAmount number(10,2),
-	InstallationCharges number(10,2)
-)
+	ID numeric(10,0) PRIMARY KEY,
+	stbtype varchar(255),
+	features varchar(255),
+	dimensions varchar(255),
+	price numeric(10,2),
+	installationCharges numeric(10,2),
+	upgrationCharges numeric(10,2),
+	discount numeric(5,2),
+	billingType varchar(255),
+	refundableAmount numeric(10,2)
+);
 
-insert into SetUpBoxTypesBox values (1, '30 x 20 x 5', 1, null, 100.00, 30.00, 0.20, null, 50.00, 50.00);
-insert into SetUpBoxTypesBox values (2, '30 x 20 x 5', 2, '', 200.00, 30.00, 0.20, null, 150.00, 50.00);
-insert into SetUpBoxTypesBox values (3, '30 x 20 x 5', 3, '', 300.00, 30.00, 0.20, null, 250.00, 50.00);
-insert into SetUpBoxTypesBox values (4, '30 x 20 x 5', 4, '', 400.00, 30.00, 0.20, null, 300.00, 50.00);
-
-
-
-
-
+insert into SetUpBoxTypesBox values (1, 'Standard', '','30 x 20 x 5', 4000.00, 150.50, 0, 2.0, 'Post Paid', 1000);
+insert into SetUpBoxTypesBox values (2, 'High Definition (HD)', '', '30 x 20 x 5', 5500.00, 100.00, 100, 2.2, 'Pre Paid', 0);
+insert into SetUpBoxTypesBox values (3, 'High Definition Recorder HD+', '', '30 x 20 x 5', 6000.00, 50.00, 170, 4.0, 'Pre Paid', 0);
+insert into SetUpBoxTypesBox values (4, 'IPTV', '','30 x 20 x 5', 7500.00, 50.00, 1000, 10, 'Post Paid', 1000);
