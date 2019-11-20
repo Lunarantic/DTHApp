8cf44440038a4dd9a492c69673e20311aa88f596
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2019-11-20 15:32:37 -0500

    To have single unified flow under same url

--- src/servlets/Main.java
+++ src/servlets/Main.java
@@ -1,24 +1,18 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import util.DatabaseUtil;
 
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-//	private static final String username = "Rahul";
-//	private static final String password = "journaldev";
     
     public Main() {
         super();
@@ -33,32 +27,28 @@ public class Main extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
-		if(username.isEmpty() || password.isEmpty() )
-		{
-//			RequestDispatcher req = request.getRequestDispatcher("login.jsp");
-//			System.out.println("No username");
-//			req.include(request, response);
-//			HttpSession session = request.getSession();
-//			session.setAttribute("username", username);
-			RequestDispatcher req = request.getRequestDispatcher("./");
-			System.out.println("No username");
-			req.forward(request, response);
-		}
-		else
-		{
-			Cookie loginCookie = new Cookie("username",username);
-			//setting cookie to expiry in 30 mins
-			loginCookie.setMaxAge(30*60);
-			System.out.println("Login Success");
-			response.addCookie(loginCookie);
-			response.sendRedirect("./welcome");
+		String action = request.getParameter("action");
+		
+		if (action == null) {
+			request.getSession().getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+		} else if (action.equals("login")) {
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			
+			if(username.isEmpty() || password.isEmpty()) {
+				request.getSession().getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
+			} else {
+				Cookie loginCookie = new Cookie("username",username);
+				loginCookie.setMaxAge(30*60); // 30 mins
+				response.addCookie(loginCookie);
+				request.getSession().getServletContext().getRequestDispatcher("/jsp/login_success.jsp").forward(request, response);
+			}
+		} else {
+			request.getSession().getServletContext().getRequestDispatcher("/jsp/index.jsp").forward(request, response);
 		}
 	}
 }
