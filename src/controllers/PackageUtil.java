3f4b98ddb6d40e223b88f7eb4152abb1fbcee0e7
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2019-11-21 16:56:22 -0500

    performance improvement of queries

--- src/controllers/PackageUtil.java
+++ src/controllers/PackageUtil.java
@@ -11,62 +11,65 @@ public class PackageUtil {
 
 	public List<List<String>> getDefualtPkgs() throws SQLException {
 		List<List<String>> list = new ArrayList<>();
-		String getDefaultSql = "select * from Packages";
+		
+		String getDefaultSql = "select name, id, datefrom from packages";
 		PreparedStatement st = DatabaseUtil.getConnection().prepareStatement(getDefaultSql);
 		ResultSet rs = st.executeQuery();
+		
 		while(rs.next()) {
 			List<String> curr = new ArrayList<>();
-			curr.add(rs.getString("name"));
-			curr.add(rs.getString("id"));
-			curr.add(rs.getString("Datefrom"));
+			curr.add(rs.getString(1));
+			curr.add(rs.getString(2));
+			curr.add(rs.getString(3));
 			list.add(curr);
 		}
+		
 		DatabaseUtil.close(rs, st);
 		return list;
 	}
 	
 	public List<List<String>> getChannels(String packageID) throws SQLException {
 		List<List<String>> list = new ArrayList<>();
-		String getDefaultSql = "select * from PackageChannels where PackageID=?";
+		
+		String getDefaultSql = "select c.name, c.charge from channels c inner join packagechannels p on c.id = p.channelid where p.packageid=?";
+		
 		PreparedStatement st = DatabaseUtil.getConnection().prepareStatement(getDefaultSql);
 		st.setString(1, packageID);
-		ResultSet rs = st.executeQuery();
-		List<String> channels = new ArrayList<>();
-		while(rs.next()) {
-			channels.add(rs.getString("ChannelID"));
-		}
-		DatabaseUtil.close(rs, st);
-		for(String channelID : channels) {
-			String getChannelSql = "select * from Channels where id=? ";
-			st = DatabaseUtil.getConnection().prepareStatement(getChannelSql);
-			st.setString(1, channelID);
-			ResultSet rsChannel = st.executeQuery();
-			while(rsChannel.next()) {
-					List<String> curr = new ArrayList<>();
-					curr.add(rsChannel.getString("name"));
-					curr.add(rsChannel.getString("charge"));
-					list.add(curr);
-				}
-			DatabaseUtil.close(rsChannel, st);
-		}
 		
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()) {
+			List<String> curr = new ArrayList<>();
+			curr.add(rs.getString(1));
+			curr.add(rs.getString(2));
+			list.add(curr);
+		}
+
+		DatabaseUtil.close(rs, st);
 		return list;
 	}
 	
 	public List<List<String>> getCustomerPkg(int customerID) throws SQLException {
 		List<List<String>> list = new ArrayList<>();
-		String sql = "select * from Packages p, PackageSales ps where ps.customer_id=? and p.id = ps.Package_id";
+		
+		String sql = "select name, category, start_date, package_id"
+				+ " from Packages p, PackageSales ps"
+				+ " where ps.customer_id=? and p.id = ps.Package_id";
+		
 		PreparedStatement st = DatabaseUtil.getConnection().prepareStatement(sql);
 		st.setInt(1, customerID);
+		
 		ResultSet rs = st.executeQuery();
 		while(rs.next()) {
 			List<String> pkg = new ArrayList<>();
-			pkg.add(rs.getString("name"));
-			pkg.add(rs.getString("category"));
-			pkg.add(rs.getString("Start_date"));
-			pkg.add(rs.getString("Package_id"));
+			pkg.add(rs.getString(1));
+			pkg.add(rs.getString(2));
+			pkg.add(rs.getString(3));
+			pkg.add(rs.getString(4));
 			list.add(pkg);
 		}
+		
+		DatabaseUtil.close(rs, st);
 		return list;
 		
 	}
