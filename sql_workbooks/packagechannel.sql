2811ec4769fba7726222225747a768aa82fde29d
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2019-11-21 14:52:14 -0500

    Modified query for Channels in Packages

--- sql_workbooks/packagechannel.sql
+++ sql_workbooks/packagechannel.sql
@@ -6,16 +6,15 @@ CREATE table PackageChannels (
 	constraint fk_pc_chnid FOREIGN KEY (ChannelID) REFERENCES channels (id)
 );
 
-insert into PackageChannels values(1, 1);
 insert into PackageChannels values(1, 2);
-insert into PackageChannels values(1, 3);
-insert into PackageChannels values(1, 4);
-insert into PackageChannels values(1, 5);
-insert into PackageChannels values(1, 6);
-insert into PackageChannels values(1, 7);
-insert into PackageChannels values(2, 8);
-insert into PackageChannels values(2, 9);
-insert into PackageChannels values(2, 10);
-insert into PackageChannels values(2, 11);
-insert into PackageChannels values(2, 12);
-
+insert into PackageChannels values(1, 8);
+insert into PackageChannels values(1, 16);
+insert into PackageChannels values(2, 6);
+insert into PackageChannels values(2, 7);
+insert into PackageChannels values(3, 1);
+insert into PackageChannels values(3, 3);
+insert into PackageChannels values(4, 12);
+insert into PackageChannels values(4, 20);
+insert into PackageChannels values(5, 14);
+insert into PackageChannels values(6, 11);
+insert into PackageChannels values(6, 19);
